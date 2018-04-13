bf9cf74ab83e908745bbe981faac622194ef43da
Romeski <controltower@Romeskis-MBP.fios-router.home>
2018-04-13 12:39:09 -0400

    ec2 extension

--- testering.tf
+++ testering.tf
@@ -1,6 +1,10 @@
 provider "aws" {}
 
 resource "aws_instance" "example" {
-    ami =   "ami-2757f631"
+    ami =   "ami-b374d5a5"
     instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.example.id}"
 }
