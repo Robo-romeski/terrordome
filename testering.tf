3ecbaf56b6bb82687d4b87f73607eaa1380bb0ec
Romeski <controltower@Romeskis-MBP.fios-router.home>
2018-04-13 12:43:11 -0400

    eip link

--- testering.tf
+++ testering.tf
@@ -1,10 +1,10 @@
 provider "aws" {}
 
-resource "aws_instance" "terradome" {
+resource "aws_instance" "terrordome" {
     ami =   "ami-b374d5a5"
     instance_type = "t2.micro"
 }
 
 resource "aws_eip" "ip" {
-  instance = "${aws_instance.terradome.id}"
+  instance = "${aws_instance.terrordome.id}"
 }
